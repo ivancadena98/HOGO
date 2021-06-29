4470abc5872329fb952f40a27e309d9c20196139
ivancadena98 <ivancadena98@outlook.com>
2021-06-29 15:12:59 -0500

    Email form update

--- sendEmail.php
+++ sendEmail.php
@@ -2,11 +2,11 @@
 $name = $_POST['Name'];
 $mail = $_POST['Email'];
 $message = $_POST['Content'];
-$addressee = 'ivancadena98@outlook.com';
+$addressee = 'contacto@hogoepp.com';
 $topic = 'HOGO Cliente';
 
-$headers .=  'MIME-Version: 1.0' . "\r\n"; 
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+$header =  'MIME-Version: 1.0' . "\r\n"; 
+$header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
 $header .= 'Enviado desde la pagina de HOGO';
 
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
@@ -14,9 +14,6 @@ $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Mensaje: " . $_POST['Content'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'ivancadena98@outlook.com';
-
 mail($addressee, $topic,$message, $header);
-echo "<script>alert('enviado')</script>";
 echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
 ?>
